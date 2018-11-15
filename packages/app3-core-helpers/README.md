469a8e947b4bd8d45bd7ab82ee6902b3009833d7
Daniel <danieluckee@gmail.com>
2018-11-15 13:30:19 +0900

    added app3-core-helpers

---
+++ packages/app3-core-helpers/README.md
@@ -0,0 +1,31 @@
+# app3-core-helpers
+
+This is a sub package of [app3.js][repo]
+
+Helper functions used in [app3.js][repo] packages.
+Please read the [documentation][docs] for more.
+
+## Installation
+
+### Node.js
+
+```bash
+npm install app3-core-helpers
+```
+
+## Usage
+
+```js
+// in node.js
+var helpers = require('app3-core-helpers');
+
+helpers.formatters;
+helpers.errors;
+...
+```
+
+
+[docs]: https://app3js.readthedocs.io/en/latest
+[repo]: https://github.com/APIS-Platform/app3.js
+
+
---
+++ packages/app3-core-helpers/src/index.js
@@ -0,0 +1,33 @@
+/*
+    This file is part of app3.js.
+
+    app3.js is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    app3.js is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with app3.js.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/**
+ * @file index.js
+ * The index.js file has been modified by the APIS team from the web3.js.
+ *
+ * @date 2018.11
+ */
+
+"use strict";
+
+var errors = require('./errors');
+var formatters = require('./formatters');
+
+module.exports = {
+    errors: errors,
+    formatters: formatters
+};
+
