18e4bbf2d347e9fbf200f3c7d1777d477c80b443
Daniel <danieluckee@gmail.com>
2019-01-04 17:21:21 +0900

    Added processing when toMask value is empty.

--- packages/app3-apis-accounts/src/index.js
+++ packages/app3-apis-accounts/src/index.js
@@ -179,7 +179,7 @@ Accounts.prototype.signTransaction = function signTransaction(tx, privateKey, ca
             transaction.data = tx.data || '0x';
             transaction.value = tx.value || '0x';
             transaction.chainId = utils.numberToHex(tx.chainId);
-            transaction.toMask = utils.utf8ToHex(tx.toMask) || '';
+            transaction.toMask = tx.toMask ? (utils.utf8ToHex(tx.toMask) || '') : '';
 
             var rlpEncoded = RLP.encode([
                 Bytes.fromNat(transaction.nonce),
